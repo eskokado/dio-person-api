c8fc7c7ee6e3322c79a0859c4740a71d042481f6
Edson Shideki Kokado <eskokado@gmail.com>
2020-12-19 20:13:11 -0300

    Initial commit

---
+++ src/main/java/com/dio/diopersonapi/DioPersonApiApplication.java
@@ -0,0 +1,13 @@
+package com.dio.diopersonapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioPersonApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioPersonApiApplication.class, args);
+	}
+
+}
