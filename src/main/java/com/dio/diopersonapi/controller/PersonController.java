f0eaef1aac020169e9713c149a5bc8aa8b35d465
Edson Shideki Kokado <eskokado@gmail.com>
2020-12-20 15:26:46 -0300

    Refactory constructor autowired with lombok and add README.md

--- src/main/java/com/dio/diopersonapi/controller/PersonController.java
+++ src/main/java/com/dio/diopersonapi/controller/PersonController.java
@@ -4,6 +4,7 @@ import com.dio.diopersonapi.dto.request.PersonDTO;
 import com.dio.diopersonapi.dto.response.MessageResponseDTO;
 import com.dio.diopersonapi.exception.PersonNotFoundException;
 import com.dio.diopersonapi.service.PersonService;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -13,14 +14,10 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/people")
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class PersonController {
 
     private PersonService personService;
-
-    @Autowired
-    public PersonController(PersonService personService) {
-        this.personService = personService;
-    }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
